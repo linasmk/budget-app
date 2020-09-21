ec8d35a14010fbb917450a352b9805d766127673
linasmk <linas.mackonis@gmail.com>
2020-09-21 13:00:14 +0200

    Refactored AddExpense page from functional to class component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -2,17 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 import { addExpense } from "../actions/expenses";
 import ExpenseForm from "./ExpenseForm";
-import { cl } from "../utils/DebugHelpers";
 
-const AddExpensePage = (props) => (
-  <article>
-    <h1>Add Expense</h1>
-    <ExpenseForm
-      onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
-        props.history.push("/");
-      }}
-    />
-  </article>
-);
-export default connect()(AddExpensePage);
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.onSubmit(expense);
+    this.props.history.push("/");
+  };
+  render() {
+    return (
+      <article>
+        <h1>Add Expense</h1>
+        <ExpenseForm onSubmit={this.onSubmit} />
+      </article>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (expense) => dispatch(addExpense(expense)),
+});
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
